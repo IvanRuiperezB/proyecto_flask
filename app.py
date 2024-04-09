cee6d92db56355dc03fa6123843673c404b08f60
Ivan Ruiperez <ivanruiperez.instituto@gmail.com>
2024-04-09 10:14:47 +0200

    recetas.html y menu

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def inicio():
     return render_template("inicio.html")
+@app.route('/recetas')
+def recetas():
+    return render_template("recetas.html")
+
+app.run("0.0.0.0",5000,debug=True)
