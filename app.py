c543067ac05cf7fc6b6e4557b6e4d3347d0f486e
Ivan Ruiperez <ivanruiperez.instituto@gmail.com>
2024-04-16 09:04:00 +0200

    listarecetas

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def recetas():
 @app.route('/listarecetas', methods=["POST"])
 def listarecetas():
     receta=request.form.get("receta")
-    return render_template("listarecetas.html",receta=receta)
+    with open("Data_Recipes.json") as fich:
+        datos=json.load(fich)
+    recetas=[]
+    for item in datos:
+        if receta.lower() in item["name"].lower():
+            if item["name"] not in recetas:
+                receta1={"name":item["name"],"autor":item["author"],"detalles":item["_id"]["$oid"]}
+                recetas.append(receta1)
+    return render_template("listarecetas.html",recetas=recetas)
 app.run("0.0.0.0",5000,debug=True)
