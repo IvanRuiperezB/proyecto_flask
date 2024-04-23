fb38fbdb133b0c78160105d903482eefef824d55
Ivan Ruiperez <ivanruiperez.instituto@gmail.com>
2024-04-23 10:02:46 +0200

    terminado

--- app.py
+++ app.py
@@ -70,6 +70,4 @@ def receta():
                 pasos=0
             return render_template("receta.html",recetas=recetas,ingredientes=ingredientes,pasos=pasos)
     return abort(404)
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+app.run("0.0.0.0",5000,debug=True)
